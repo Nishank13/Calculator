2600dedead0891d473b6dd3fbd955b45d3d626d9
Nishank13 <48673375+Nishank13@users.noreply.github.com>
2020-03-16 00:46:34 +0530

    Some major changes
    Several features have been added, eg:
          > in display area string containing alphabets is not allowed to be pasted.
          > alphabets through keyboards are not allowed.
          > some UI changes.
          > steps area is added where one can scroll through previous results.

--- calculator.js
+++ calculator.js
@@ -1,69 +1,92 @@
+/*function for not allowing unsual paste of characters in display area*/
+function noPaste(obj){
+	document.getElementById('textarea').addEventListener('paste', handlePaste);
+}
+/*function for checking content of text to be pasted*/
+function handlePaste (e) {
+    var clipboardData, pastedData;
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text');
+ 	pastedData=pastedData.split("");
+ 	var flag=0;
+ 	for (var i = 0; i < pastedData.length; i++) {
+ 		if(check(pastedData[i])== "false"){
+ 			e.stopPropagation();
+    		e.preventDefault();
+ 			reset('ce');
+ 			break;
+ 		}
+ 	}
+ 	/*if (flag==0) {
 
-/*document.getElementById("textarea").addEventListener("keyup", function(display(e));*/
-/*{
-  event.preventDefault();
-  alert("Was preventDefault() called: " + event.defaultPrevented);
-});*/
+ 	}*/
+ function check(item){
+	var ar2=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+		if ((ar2.includes(item))){
+			alert("Alphabets are not allowed!!!!");
+			flag=1;
+			return("false");
+		}
+		else{
+			return ("true");
+		}
+    }
+}
+/*function created to allow only numbers and special characters to be entered thorugh keyboard*/
 function num_all(a){
 	var evnt = event || a;
-	console.log(evnt);
 	var ar,ar1;
 	ar = [48,49,50,51,52,53,54,55,56,57,8,13,46,47,42,40,41,94,45,43,37];//for without numpad 
 	ar1 = [48,49,50,51,52,53,54,55,56,57,8,13,110,111,106,219,221,27,96,97,98,99,100,101,102,103,104,105,106,107,109,110,111]; //for with numpad 
-	console.log(evnt.keyCode);
-	/*console.log(ar);*/
 	var qw=ar.includes(evnt.keyCode) || ar1.includes(evnt.keycode) ;
-	console.log(qw);
+	if (evnt.keyCode == 13) {
+		solve();
+	}
 	if (qw!=true) {
-	/*console.log('qw');*/
 		return false;
-	}
-/*	ar.forEach(dontwrite);
-	
-	let f = e.keycode;
-	if (!(for f in ar)) {
-		this.preventDefault();
-		alert("action was prevented");
-	}
-	function dontwrite(value){
-		/*alert("h");
-		if ( value == a.keycode ) {
-			this.preventDefault();
-			alert("prevented");
-		}
-	}*/
-	
+	}	
 }
+/*when enter key is pressed while focus is on display area*/
+function task(e){
+	if(e.keyCode==13){
+		solve();}
+}
+/*function to simply append the numerics*/
 function display(e){
 		document.getElementById("textarea").value += e;
 }
+/*if last character is another symbol then is it replaced through this function*/
 function cal(o){
-	//var arr = ['/','*','+','-'];
 	var vari=document.getElementById("textarea").value;
 	var y=vari.length;
 	var ex=vari[y-1];
-	//var ex=ex-1;
 	if( ex=='+' || ex=='-' || ex=='*' || ex=='/' || ex=='^' ) {  
 		vari=vari.substring(0,vari.length-1);
-		//document.getElementById("textarea").innerHTML=vari;	
 		document.getElementById("textarea").value =  vari + o;	
 	}
 	else{
 		document.getElementById("textarea").value += o;
 	}
 }
+/*checking the last character if it is a symbol then it is removed and the result is evaluated*/
 function solve(){
 	vari=document.getElementById("textarea").value;
 	lst_ele=vari.substring(vari.length-1);
 	if (lst_ele=='+' || lst_ele=='-' || lst_ele=='*' || lst_ele=='/' || lst_ele=='^') {
 		let z=vari.substring(0,vari.length-1);	
 		let a=eval(z);
-		document.getElementById("textarea").value=a;}
+		document.getElementById("textarea").value=a;
+		history(a);}
+	else if(vari==''){
+		document.getElementById("textarea").value=0;
+	}
 	else{
 		let x=document.getElementById("textarea").value;  
 		let y= eval(x);
-		document.getElementById("textarea").value=y;}
+		document.getElementById("textarea").value=y;
+		history(y);}
 }	
+/*created to either remove the last character or reset the whole text in display area*/
 function reset(e){
 	if (e=='ce') {
 		document.getElementById("textarea").value="";}
@@ -71,44 +94,16 @@ function reset(e){
 		let vari = document.getElementById("textarea").value;
 		vari = vari.substring(0,vari.length -1);
 		document.getElementById("textarea").value=vari;
-		/*document.getElementById("textarea").value += "";*/
 	}	
 }
-/*function blinking(){
-	setInterval(blinktext, 500);
-        var txt = "";
-        var count = 0;
-        function blinktext() {
-            var cntrl = document.getElementById("txtblinkingtext");
-            if (count == 0)
-                txt = cntrl.value;
-            if (count % 2 == 0)
-                cntrl.value = "";
-            else
-                cntrl.value = txt;
-            count++;
-        }
-        let i= setInterval(pointer_blink, 2000);
-        let count=1;
-        function pointer_blink(){
-           	if(!count%2){document.getElementById("textarea").value += '|'; }
-           	else{let vari =document.getElementById("textarea").value;
-        	document.getElementById("textarea").value = vari.substring(0,vari.length-1); 
-        }
-        	count++;
-		    /*let vari =document.getElementById("textarea").value ;
-			let cur=vari.substring(vari.length-1);
-			document.getElementById("textarea").value += vari;
-		    vari="" ;
-			document.getElementById("textarea").value += vari;
-			if(count>100){
-				clearInterval(i);
-			}
-		}
-}*/
-/*ar = [48(0),49(1),50(2),51(3),52(4),53(5),54(6),55(7),56(8),57(9),8(backspace),13(enter),(46.),47/,42*,40(,41),94^,45-,43+,37%];//for without numpad 
-	ar = [48(0),49(1),50(2),51(3),52(4),53(5),54(6),55(7),56(8),57(9),8(backspace),13(enter),(110.),(111/),(106*),219(,221),27esc] //for with numpad /*96,97,98,99,100,101,102,103,104,105,106,107,109,110,111*/
-////add blinking pointer------>>>>>done
-////after solving begin with zero value
-////if pressed C delete the last value------->>>>done
-////if last char is point, and next char is operator then add 2 zeroes at the end 
+/*to display the result and previous steps of user*/
+var hstry = "";
+function history(vari){
+	hist=document.getElementById("steps_area").value;
+	hist = hist + vari + "\n" ;
+	document.getElementById("steps_area").value=hist + "\n";
+}
+/*to clear the steps area*/ 
+function rmv_stps(){
+	document.getElementById("steps_area").value = "";
+}
